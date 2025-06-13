28bcee9db9cc2f40f97180fe94b268916ce3eb59
Hasan Sabri <84398988+gooficat@users.noreply.github.com>
2025-06-13 16:48:00 +0100

    Update car.py

--- car.py
+++ car.py
@@ -15,45 +15,33 @@ class Motor:
         self.p1.low()
         self.p2.low()
 
-fl = Motor(12, 13)
-fr = Motor(10, 11)
+l = Motor(0, 1)
 
-bl = Motor(19, 18)
-br = Motor(21, 20)
+r = Motor(2, 3)
 
 
 
 def neutral():
-    fl.n()
-    fr.n()
-    bl.n()
-    br.n()
+    l.n()
+    r.n()
 
 def fwd():
-    fl.f()
-    fr.f()
-    bl.f()
-    br.f()
+    l.f()
+    r.f()
 
 def bck():
-    fl.b()
-    fr.b()
-    bl.b()
-    br.b()
+    l.b()
+    r.b()
     
     
 def rgt():
-    fl.f()
-    fr.b()
-    bl.f()
-    br.b()
+    l.f()
+    r.b()
     
     
 def lft():
-    fl.b()
-    fr.f()
-    bl.b()
-    br.f()
+    l.b()
+    r.f()
 
 
 
